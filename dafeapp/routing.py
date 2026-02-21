9fcf207d93393a4947cb08cfa5599ca05132d256
gibeongideon <kipngeno.gibeon@gmail.com>
2026-02-21 21:40:10 +0300

    basic setup modules

---
+++ dafeapp/routing.py
@@ -0,0 +1,7 @@
+# Central WebSocket URL routing
+# Add app-level websocket routes here as you build them
+# Example:
+#   from monitoring.routing import websocket_urlpatterns as monitoring_ws
+#   websocket_urlpatterns = monitoring_ws + [...]
+
+websocket_urlpatterns = []
